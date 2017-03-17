6a2fb101bd7bbe179dbf387f097dd5d73553e20a
Andrew Kempen <akemperfi@gmail.com>
2017-03-17 16:58:48 -0500

    Added 3rd party libs and test code

---
+++ cmake-build-debug/CMakeFiles/COREVisionLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COREVisionLib.dir/main.cpp.o"
+  "COREVisionLib.pdb"
+  "COREVisionLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COREVisionLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
